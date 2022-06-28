900cff34ffb4f10df9834db3b84dc27d592b9e72
rauqel <ralfonso2021@elam.co.uk>
2022-06-28 12:47:22 +0100

    barriers + player
    made player control with left and right arrow keys and and a barrier that destroys piece by piece when hit by an enemy bullet (bullet not completely scripted/designed)

---
+++ space invaders REUP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D playerObject;
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementValueX = Input.GetAxis("Horizontal");
+
+        playerObject.velocity = new Vector2(movementValueX, playerObject.velocity.y) * playerSpeed;
+    }
+}
